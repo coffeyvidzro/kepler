76410210a095b35ae5dd9fc39afad6336c3ecac9
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-04 22:02:10 +0000

    Use refactored HTTP middleware

--- internal/modules/mfa/routes.go
+++ internal/modules/mfa/routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/labstack/echo/v5"
 
 	"github.com/coffeyvidzro/dugble/server/internal/platform/authnz"
-	"github.com/coffeyvidzro/dugble/server/internal/transport/middlewares"
+	httpmiddleware "github.com/coffeyvidzro/dugble/server/internal/transport/http/middleware"
 )
 
 func RegisterRoutes(router *echo.Echo, handler *Handler, authMiddleware, csrfMiddleware echo.MiddlewareFunc) {
@@ -14,8 +14,8 @@ func RegisterRoutes(router *echo.Echo, handler *Handler, authMiddleware, csrfMid
 	group.Use(authMiddleware)
 	group.Use(csrfMiddleware)
 
-	recentPassword := middlewares.RequireRecentAuthentication(middlewares.StepUpConfig{Assurance: authnz.AssuranceLevelOne, MaxAge: 15 * time.Minute})
-	recentMFA := middlewares.RequireRecentAuthentication(middlewares.StepUpConfig{Assurance: authnz.AssuranceLevelTwo, MaxAge: 15 * time.Minute})
+	recentPassword := httpmiddleware.RequireRecentAuthentication(httpmiddleware.StepUpConfig{Assurance: authnz.AssuranceLevelOne, MaxAge: 15 * time.Minute})
+	recentMFA := httpmiddleware.RequireRecentAuthentication(httpmiddleware.StepUpConfig{Assurance: authnz.AssuranceLevelTwo, MaxAge: 15 * time.Minute})
 	group.GET("", handler.Status)
 	group.POST("/totp/enroll", handler.Enroll, recentPassword)
 	group.POST("/totp/confirm", handler.Confirm, recentPassword)
