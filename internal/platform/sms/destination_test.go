f6ae274ea361a282a71befb516cf1b2d1657fde2
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-05 12:54:51 +0000

    Test Ghana and Nigeria destination catalog

--- internal/platform/sms/destination_test.go
+++ internal/platform/sms/destination_test.go
@@ -1,6 +1,7 @@
 package sms_test
 
 import (
+	"errors"
 	"testing"
 
 	platformsms "github.com/coffeyvidzro/dugble/server/internal/platform/sms"
@@ -12,13 +13,24 @@ func TestSupportedDestinationCountryUsesResolutionCatalog(t *testing.T) {
 	if !platformsms.IsSupportedDestinationCountry("gh") {
 		t.Fatal("IsSupportedDestinationCountry(gh) = false")
 	}
-	if platformsms.IsSupportedDestinationCountry("ZA") {
-		t.Fatal("IsSupportedDestinationCountry(ZA) = true")
+	if !platformsms.IsSupportedDestinationCountry("ng") {
+		t.Fatal("IsSupportedDestinationCountry(ng) = false")
+	}
+	if platformsms.IsSupportedDestinationCountry("KE") {
+		t.Fatal("IsSupportedDestinationCountry(KE) = true")
+	}
+
+	country, err := platformsms.ResolveDestinationCountry("+2348000000000")
+	if err != nil || country != platformsms.CountryNigeria {
+		t.Fatalf("ResolveDestinationCountry(NG) = %q, %v", country, err)
+	}
+	if _, err := platformsms.ResolveDestinationCountry("+254700000001"); !errors.Is(err, platformsms.ErrUnsupportedDestination) {
+		t.Fatalf("ResolveDestinationCountry(KE) error = %v", err)
 	}
 
 	destinations := platformsms.SupportedDestinations()
-	if len(destinations) == 0 {
-		t.Fatal("SupportedDestinations() returned no destinations")
+	if len(destinations) != 2 {
+		t.Fatalf("SupportedDestinations() count = %d, want 2", len(destinations))
 	}
 	destinations[0].CountryCode = "XX"
 	if !platformsms.IsSupportedDestinationCountry(platformsms.CountryGhana) {
