fe5a76cc583b20b1bbf73a056ef39ee670df77c7
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-05 12:46:05 +0000

    Expose shared SMS destination catalog

--- internal/platform/sms/destination.go
+++ internal/platform/sms/destination.go
@@ -1,76 +1,36 @@
 package sms
 
-import (
-	"errors"
-	"regexp"
-	"strings"
-)
+import "github.com/coffeyvidzro/dugble/server/internal/platform/sms/destination"
 
 const (
-	CountryGhana   = "GH"
-	CountryKenya   = "KE"
-	CountryNigeria = "NG"
+	CountryGhana   = destination.CountryGhana
+	CountryKenya   = destination.CountryKenya
+	CountryNigeria = destination.CountryNigeria
 )
 
 var (
-	ErrInvalidE164            = errors.New("invalid E.164 phone number")
-	ErrUnsupportedDestination = errors.New("unsupported SMS destination country")
-	e164Pattern               = regexp.MustCompile(`^\+[1-9]\d{7,14}$`)
+	ErrInvalidE164            = destination.ErrInvalidE164
+	ErrUnsupportedDestination = destination.ErrUnsupportedDestination
 )
 
-type Destination struct {
-	CallingCode string
-	CountryCode string
-}
-
-var supportedDestinations = []Destination{
-	{CallingCode: "+233", CountryCode: CountryGhana},
-	{CallingCode: "+234", CountryCode: CountryNigeria},
-	{CallingCode: "+254", CountryCode: CountryKenya},
-}
+type Destination = destination.Destination
 
 func SupportedDestinations() []Destination {
-	result := make([]Destination, len(supportedDestinations))
-	copy(result, supportedDestinations)
-	return result
+	return destination.Supported()
 }
 
 func ResolveDestinationCountry(number string) (string, error) {
-	number = strings.TrimSpace(number)
-	if !e164Pattern.MatchString(number) {
-		return "", ErrInvalidE164
-	}
-	for _, destination := range supportedDestinations {
-		if strings.HasPrefix(number, destination.CallingCode) {
-			return destination.CountryCode, nil
-		}
-	}
-	return "", ErrUnsupportedDestination
+	return destination.ResolveCountry(number)
 }
 
 func NormalizeCountryCode(value string) string {
-	return strings.ToUpper(strings.TrimSpace(value))
+	return destination.NormalizeCountryCode(value)
 }
 
 func IsCountryCode(value string) bool {
-	value = NormalizeCountryCode(value)
-	if len(value) != 2 {
-		return false
-	}
-	for _, character := range value {
-		if character < 'A' || character > 'Z' {
-			return false
-		}
-	}
-	return true
+	return destination.IsCountryCode(value)
 }
 
 func IsSupportedDestinationCountry(value string) bool {
-	value = NormalizeCountryCode(value)
-	for _, destination := range supportedDestinations {
-		if destination.CountryCode == value {
-			return true
-		}
-	}
-	return false
+	return destination.IsSupportedCountry(value)
 }
