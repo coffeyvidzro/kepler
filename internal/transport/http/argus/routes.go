d3f13e1298a20e8a3476fcac9a621a68c0d9a323
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-06 03:23:22 +0000

    remove verification service routes

--- internal/transport/http/argus/routes.go
+++ internal/transport/http/argus/routes.go
@@ -9,12 +9,6 @@ import (
 type AccessMiddleware func(tenant.Permission) echo.MiddlewareFunc
 
 func RegisterRoutes(router *echo.Echo, handler *Handler, access AccessMiddleware) {
-	services := router.Group("/verification-services")
-	services.POST("", handler.CreateService, access(tenant.PermissionVerifyManage))
-	services.GET("", handler.ListServices, access(tenant.PermissionVerifyRead))
-	services.GET("/:service_id", handler.GetService, access(tenant.PermissionVerifyRead))
-	services.PATCH("/:service_id", handler.UpdateService, access(tenant.PermissionVerifyManage))
-
 	verifications := router.Group("/verifications")
 	verifications.POST("", handler.Create, access(tenant.PermissionVerifySend))
 	verifications.GET("", handler.List, access(tenant.PermissionVerifyRead))
