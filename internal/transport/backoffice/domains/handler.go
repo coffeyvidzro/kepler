11b53775645012c72b5a466b36b44c9366f51a6f
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-06 18:39:56 +0000

    Render backoffice domains as read-only pages

--- internal/transport/backoffice/domains/handler.go
+++ internal/transport/backoffice/domains/handler.go
@@ -8,8 +8,8 @@ import (
 	"github.com/labstack/echo/v5"
 
 	backofficedomains "github.com/coffeyvidzro/dugble/server/internal/backoffice/domains"
+	backofficehttp "github.com/coffeyvidzro/dugble/server/internal/transport/backoffice"
 	apperrors "github.com/coffeyvidzro/dugble/server/pkg/errors"
-	"github.com/coffeyvidzro/dugble/server/pkg/httputil"
 )
 
 type service interface {
@@ -21,6 +21,16 @@ type Handler struct {
 	service service
 }
 
+type listPage struct {
+	Domains        []backofficedomains.Domain
+	Limit          int32
+	Offset         int32
+	PreviousOffset int32
+	NextOffset     int32
+	HasPrevious    bool
+	HasNext        bool
+}
+
 func NewHandler(service service) *Handler {
 	return &Handler{service: service}
 }
@@ -28,28 +38,54 @@ func NewHandler(service service) *Handler {
 func (handler *Handler) List(c *echo.Context) error {
 	limit, err := parseInt32Query(c.QueryParam("limit"), "limit")
 	if err != nil {
-		return httputil.Error(c, err)
+		return err
 	}
 	offset, err := parseInt32Query(c.QueryParam("offset"), "offset")
 	if err != nil {
-		return httputil.Error(c, err)
+		return err
 	}
+
 	page, err := handler.service.List(c.Request().Context(), backofficedomains.ListInput{
 		Limit:  limit,
 		Offset: offset,
 	})
 	if err != nil {
-		return httputil.Error(c, err)
+		return err
 	}
-	return httputil.OK(c, page)
+
+	return backofficehttp.RenderPage(c, "domains.html", backofficehttp.PageData{
+		Title: "Domains",
+		Data:  newListPage(page),
+	})
 }
 
-func (handler *Handler) Get(c *echo.Context) error {
+func (handler *Handler) Detail(c *echo.Context) error {
 	domain, err := handler.service.Get(c.Request().Context(), c.Param("domain_id"))
 	if err != nil {
-		return httputil.Error(c, err)
+		return err
 	}
-	return httputil.OK(c, domain)
+
+	return backofficehttp.RenderPage(c, "domain_detail.html", backofficehttp.PageData{
+		Title: domain.Name,
+		Data:  domain,
+	})
+}
+
+func newListPage(page backofficedomains.Page) listPage {
+	previousOffset := page.Offset - page.Limit
+	if previousOffset < 0 {
+		previousOffset = 0
+	}
+
+	return listPage{
+		Domains:        page.Domains,
+		Limit:          page.Limit,
+		Offset:         page.Offset,
+		PreviousOffset: previousOffset,
+		NextOffset:     page.Offset + page.Limit,
+		HasPrevious:    page.Offset > 0,
+		HasNext:        len(page.Domains) == int(page.Limit),
+	}
 }
 
 func parseInt32Query(value string, name string) (int32, error) {
