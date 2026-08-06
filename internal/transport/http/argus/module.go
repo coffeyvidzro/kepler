14d3acd36f72a326549be1c688e3d0d782fa0b43
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-06 03:28:04 +0000

    remove verification service aliases

--- internal/transport/http/argus/module.go
+++ internal/transport/http/argus/module.go
@@ -3,8 +3,6 @@ package argus
 import argusmodule "github.com/coffeyvidzro/dugble/server/internal/modules/argus"
 
 type Service = argusmodule.Service
-type CreateServiceRequest = argusmodule.CreateServiceRequest
 type ListRequest = argusmodule.ListRequest
-type UpdateServiceRequest = argusmodule.UpdateServiceRequest
 type CreateVerificationRequest = argusmodule.CreateVerificationRequest
 type CheckRequest = argusmodule.CheckRequest
