9982939c9e39bf6f93544ae49a33aaf3017e39b9
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-06 08:23:08 +0000

    Remove Argus verification API (#38)
    * chore: stage Argus API removal
    
    * chore: run Argus removal from pull request
    
    * chore: preserve authentication verification queries
    
    * chore: remove unused Argus product runtime
    
    * remove Argus verification API
    
    * chore: trigger Argus removal checks
    
    * chore: finalize Argus removal checks
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- internal/transport/http/argus/module.go
+++
@@ -1,8 +0,0 @@
-package argus
-
-import argusmodule "github.com/coffeyvidzro/dugble/server/internal/modules/argus"
-
-type Service = argusmodule.Service
-type ListRequest = argusmodule.ListRequest
-type CreateVerificationRequest = argusmodule.CreateVerificationRequest
-type CheckRequest = argusmodule.CheckRequest
