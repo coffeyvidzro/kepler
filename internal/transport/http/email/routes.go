9be4a8750d367c429a5093c35ee22f092877e4b1
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-07 14:12:50 +0000

    register email attachment and metrics routes

--- internal/transport/http/email/routes.go
+++ internal/transport/http/email/routes.go
@@ -13,7 +13,11 @@ func RegisterRoutes(router *echo.Echo, handler *Handler, tenantAccess TenantMidd
 	emails.GET("", handler.List, tenantAccess(tenant.PermissionEmailRead))
 	emails.POST("", handler.Send, tenantAccess(tenant.PermissionEmailSend))
 	emails.POST("/batch", handler.BatchSend, tenantAccess(tenant.PermissionEmailSend))
+	emails.GET("/metrics", handler.Metrics, tenantAccess(tenant.PermissionEmailRead))
 	emails.POST("/:message_id/cancel", handler.Cancel, tenantAccess(tenant.PermissionEmailSend))
 	emails.PATCH("/:message_id", handler.Update, tenantAccess(tenant.PermissionEmailSend))
+	emails.GET("/:message_id/attachments", handler.ListAttachments, tenantAccess(tenant.PermissionEmailRead))
+	emails.GET("/:message_id/attachments/:attachment_id", handler.GetAttachment, tenantAccess(tenant.PermissionEmailRead))
+	emails.GET("/:message_id/attachments/:attachment_id/download", handler.DownloadAttachment, tenantAccess(tenant.PermissionEmailRead))
 	emails.GET("/:message_id", handler.Get, tenantAccess(tenant.PermissionEmailRead))
 }
