eaaad5355fba47d49f061d9670886f27bfde369f
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-07 06:33:06 +0000

    Add suppression HTTP module aliases

---
+++ internal/transport/http/suppression/module.go
@@ -0,0 +1,7 @@
+package suppression
+
+import suppressionmodule "github.com/coffeyvidzro/dugble/server/internal/modules/suppression"
+
+type Service = suppressionmodule.Service
+type CreateRequest = suppressionmodule.CreateRequest
+type ListRequest = suppressionmodule.ListRequest
