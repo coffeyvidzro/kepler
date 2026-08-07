3d080b1c10d704499b502732879c1bff67f43785
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-07 06:53:54 +0000

    Add message template HTTP module aliases

---
+++ internal/transport/http/messagetemplate/module.go
@@ -0,0 +1,9 @@
+package messagetemplate
+
+import module "github.com/coffeyvidzro/dugble/server/internal/modules/messagetemplate"
+
+type Service = module.Service
+
+type Template = module.Template
+
+type Version = module.Version
